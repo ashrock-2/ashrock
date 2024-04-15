0ff76bcb49377a568ff8c82173bb540616fc2a8a
이재석 <jslee01@miridih.com>
2024-04-15 18:45:25 +0100

    feat: 가장 최근의 게시글 Home에 노출

---
+++ src/utils/getMostRecentPost.ts
@@ -0,0 +1,16 @@
+import type { MarkdownInstance } from "astro";
+import type { Frontmatter } from "../types/Frontmatter";
+
+export const getMostRecentPost = async (
+  posts: MarkdownInstance<Frontmatter>[]
+) => {
+  if (posts.length > 0) {
+    posts.sort(
+      (a, b) =>
+        new Date(b.frontmatter.pub_date).getTime() -
+        new Date(a.frontmatter.pub_date).getTime()
+    );
+    return posts[0];
+  }
+  return undefined;
+};
