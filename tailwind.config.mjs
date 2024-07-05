6e7bc2d07d4f152318eaee4667d56edd46d7f5ec
이재석 <jslee01@miridih.com>
2024-07-05 17:50:37 +0900

    ring관련 스타일 tailwind css로 migration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 180s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
