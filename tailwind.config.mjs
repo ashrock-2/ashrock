875e1b3c52ea78b960cbf6d731b67478562bc8b4
OnLee3 <dlwotjr2@gmail.com>
2024-07-06 19:23:12 +0900

    회전 애니메이션 적용되지 않던 버그 해결

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,15 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      animation: {
-        "spin-slow-0": "spin 180s linear infinite",
-        "spin-slow-1": "spin 150s linear infinite",
-        "spin-slow-2": "spin 270s linear infinite",
-        "spin-slow-3": "spin 360s linear infinite",
-        "spin-slow-4": "spin 210s linear infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
   corePlugins: {
